7da6b6941a49e8017b897a9367a37e423323786a
matteodcr <mattdecorsaire@gmail.com>
2023-03-12 22:35:40 +0100

    Initial commit

---
+++ viewer.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+"""
+Python OpenGL practical application.
+"""
+
+import sys                          # for system arguments
+from itertools import cycle
+
+# External, non built-in modules
+import OpenGL.GL as GL              # standard Python OpenGL wrapper
+import numpy as np                  # all matrix manipulations & OpenGL args
+import glfw                         # lean window system wrapper for OpenGL
+
+from core import Shader, Mesh, Viewer, Node, load, Texture
+from transform import translate, identity, rotate, scale
+from texture import Texture, Textured
+
+
+
+class Axis(Mesh):
+    """ Axis object useful for debugging coordinate frames """
+    def __init__(self, shader):
+        pos = ((0, 0, 0), (1, 0, 0), (0, 0, 0), (0, 1, 0), (0, 0, 0), (0, 0, 1))
+        col = ((1, 0, 0), (1, 0, 0), (0, 1, 0), (0, 1, 0), (0, 0, 1), (0, 0, 1))
+        super().__init__(shader, attributes=dict(position=pos, color=col))
+
+    def draw(self, primitives=GL.GL_LINES, **uniforms):
+        super().draw(primitives=primitives, **uniforms)
+
+
+class Triangle(Mesh):
+    """Hello triangle object"""
+    def __init__(self, shader):
+        position = np.array(((0, .5, 0), (-.5, -.5, 0), (.5, -.5, 0)), 'f')
+        color = np.array(((1, 0, 0), (0, 1, 0), (0, 0, 1)), 'f')
+        self.color = (1, 1, 0)
+        attributes = dict(position=position, color=color)
+        super().__init__(shader, attributes=attributes)
+
+    def draw(self, primitives=GL.GL_TRIANGLES, **uniforms):
+        super().draw(primitives=primitives, global_color=self.color, **uniforms)
+
+    def key_handler(self, key):
+        if key == glfw.KEY_C:
+            self.color = (0, 0, 0)
+
+
+class Cylinder(Node):
+    """ Very simple cylinder based on provided load function """
+    def __init__(self, shader):
+        super().__init__()
+        self.add(*load('cylinder.obj', shader))  # just load cylinder from file
+
+# -------------- Example textured plane class ---------------------------------
+class TexturedPlane(Textured):
+    """ Simple first textured object """
+    def __init__(self, shader, tex_file):
+        # prepare texture modes cycling variables for interactive toggling
+        self.wraps = cycle([GL.GL_REPEAT, GL.GL_MIRRORED_REPEAT,
+                            GL.GL_CLAMP_TO_BORDER, GL.GL_CLAMP_TO_EDGE])
+        self.filters = cycle([(GL.GL_NEAREST, GL.GL_NEAREST),
+                              (GL.GL_LINEAR, GL.GL_LINEAR),
+                              (GL.GL_LINEAR, GL.GL_LINEAR_MIPMAP_LINEAR)])
+        self.wrap, self.filter = next(self.wraps), next(self.filters)
+        self.file = tex_file
+
+        # setup plane mesh to be textured
+        base_coords = ((-1, -1, 0), (1, -1, 0), (1, 1, 0), (-1, 1, 0))
+        scaled = 100 * np.array(base_coords, np.float32)
+        indices = np.array((0, 1, 2, 0, 2, 3), np.uint32)
+        mesh = Mesh(shader, attributes=dict(position=scaled), index=indices)
+
+        # setup & upload texture to GPU, bind it to shader name 'diffuse_map'
+        texture = Texture(tex_file, self.wrap, *self.filter)
+        super().__init__(mesh, diffuse_map=texture)
+
+    def key_handler(self, key):
+        # cycle through texture modes on keypress of F6 (wrap) or F7 (filtering)
+        self.wrap = next(self.wraps) if key == glfw.KEY_F6 else self.wrap
+        self.filter = next(self.filters) if key == glfw.KEY_F7 else self.filter
+        if key in (glfw.KEY_F6, glfw.KEY_F7):
+            texture = Texture(self.file, self.wrap, *self.filter)
+            self.textures.update(diffuse_map=texture)
+
+
+
+
+# -------------- main program and scene setup --------------------------------
+def main():
+    """ create a window, add scene objects, then run rendering loop """
+    viewer = Viewer()
+
+    # default color shader
+    shader = Shader("color.vert", "color.frag")
+    shaderTexture = Shader("texture.vert", "texture.frag")
+
+    # place instances of our basic objects
+    viewer.add(*[mesh for file in sys.argv[1:] for mesh in load(file, shader)])
+    if len(sys.argv) < 2:
+        viewer.add(Axis(shader))
+        # viewer.add(Node(children=[Cylinder(shader)], transform=translate(x=+2)))
+        # viewer.add(Node(children=[Triangle(shader)], transform=translate(x=-1)))
+        viewer.add(TexturedPlane(shaderTexture, "grass.png"))
+        print('Usage:\n\t%s [3dfile]*\n\n3dfile\t\t the filename of a model in'
+              ' format supported by assimp.' % (sys.argv[0],))
+
+    # start rendering loop
+    viewer.run()
+
+
+if __name__ == '__main__':
+    main()                     # main function keeps variables locally scoped
