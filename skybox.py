58fdea4e8fefeda509f208c8d302d1fda651c2a4
matteodcr <mattdecorsaire@gmail.com>
2023-03-19 15:59:32 +0100

    Refactor code

--- skybox.py
+++ skybox.py
@@ -1,62 +1,45 @@
 import OpenGL.GL as GL
 from PIL import Image
 import os
-
 from core import Mesh
+from texture import TexturedCube
 
-class TexturedCube:
-    """ Drawable mesh decorator that activates and binds OpenGL textures """
-    def __init__(self, drawable, **textures):
-        self.drawable = drawable 
-        self.textures = textures
+FILE_OPENING_CONFIG = 'RGBA'
 
-    def draw(self, primitives=GL.GL_TRIANGLES, **uniforms):
-        GL.glDepthFunc(GL.GL_LEQUAL);
-        for index, (name, texture) in enumerate(self.textures.items()):
-            GL.glActiveTexture(GL.GL_TEXTURE0 + index)
-            GL.glBindTexture(texture.type, texture.glid)
-            GL.glDrawArrays(GL.GL_TRIANGLES, 0, 36)
-            GL.glBindVertexArray(0)
-            uniforms[name] = index
-        self.drawable.draw(primitives=primitives, **uniforms)
-        GL.glDepthFunc(GL.GL_LESS); # set depth function back to default
 
-class CubeMapTexture(TexturedCube):
-    """ Simple first textured object """
+class SkyBox(TexturedCube):
+    """Create and texture a SkyBox derived from a TexturedCube"""
     def __init__(self, shader, tex_path):
         self.file = tex_path
+        coords = ((-1, 1, -1), (-1, -1, -1), (1, -1, -1), (1, -1, -1), (1, 1, -1), (-1, 1, -1)) + \
+                 ((-1, -1, 1), (-1, -1, -1), (-1, 1, -1), (-1, 1, -1), (-1, 1, 1), (-1, -1, 1)) + \
+                 ((1, -1, -1), (1, -1, 1), (1, 1, 1), (1, 1, 1), (1, 1, -1), (1, -1, -1)) + \
+                 ((-1, -1, 1), (-1, 1, 1), (1, 1, 1), (1, 1, 1), (1, -1, 1), (-1, -1, 1)) + \
+                 ((-1, 1, -1), (1, 1, -1), (1, 1, 1), (1, 1, 1), (-1, 1, 1), (-1, 1, -1)) + \
+                 ((-1, -1, -1), (-1, -1, 1), (1, -1, -1), (1, -1, -1), (-1, -1, 1), (1, -1, 1))
 
-        base_coords = ((-1, 1, -1), (-1, -1, -1), (1, -1, -1), (1, -1, -1), (1, 1, -1), (-1, 1, -1)) + \
-                      ((-1, -1, 1), (-1, -1, -1), (-1, 1, -1), (-1, 1, -1), (-1, 1, 1), (-1, -1, 1)) + \
-                      ((1, -1, -1), (1, -1, 1), (1, 1, 1), (1, 1, 1), (1, 1, -1), (1, -1, -1)) + \
-                      ((-1, -1, 1), (-1, 1, 1), (1, 1, 1), (1, 1, 1), (1, -1, 1), (-1, -1, 1)) + \
-                      ((-1, 1, -1), (1, 1, -1), (1, 1, 1), (1, 1, 1), (-1, 1, 1), (-1, 1, -1)) + \
-                      ((-1, -1, -1), (-1, -1, 1), (1, -1, -1), (1, -1, -1), (-1, -1, 1), (1, -1, 1))
-        mesh = Mesh(shader, attributes=dict(position=base_coords))
-
-        class CubeMapTex:
+        class Texture:
             def __init__(self, tex_path):
                 self.glid = GL.glGenTextures(1)
                 self.type = GL.GL_TEXTURE_CUBE_MAP
                 i = 0
-                for filename in sorted(os.listdir(tex_path)):
-
+                for file in sorted(os.listdir(tex_path)):
                     # Load the texture
-                    tex_file = os.path.join(tex_path, filename)
-                    tex = Image.open(tex_file).convert('RGBA')
+                    tex_file = os.path.join(tex_path, file)
+                    tex = Image.open(tex_file).convert(FILE_OPENING_CONFIG)
                     GL.glBindTexture(self.type, self.glid)
                     GL.glTexImage2D(GL.GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL.GL_RGBA, tex.width, tex.height,
                                     0, GL.GL_RGBA, GL.GL_UNSIGNED_BYTE, tex.tobytes())
 
                     # Set parameters to smooth the limit between skybox textures
-                    GL.glTexParameteri(GL.GL_TEXTURE_CUBE_MAP, GL.GL_TEXTURE_MIN_FILTER, GL.GL_LINEAR);
-                    GL.glTexParameteri(GL.GL_TEXTURE_CUBE_MAP, GL.GL_TEXTURE_MAG_FILTER, GL.GL_LINEAR);
-                    GL.glTexParameteri(GL.GL_TEXTURE_CUBE_MAP, GL.GL_TEXTURE_WRAP_S, GL.GL_CLAMP_TO_EDGE);
-                    GL.glTexParameteri(GL.GL_TEXTURE_CUBE_MAP, GL.GL_TEXTURE_WRAP_T, GL.GL_CLAMP_TO_EDGE);
-                    GL.glTexParameteri(GL.GL_TEXTURE_CUBE_MAP, GL.GL_TEXTURE_WRAP_R, GL.GL_CLAMP_TO_EDGE);
-                    print(f'Loaded texture {tex_file} ({tex.width}x{tex.height}')
+                    GL.glTexParameteri(GL.GL_TEXTURE_CUBE_MAP, GL.GL_TEXTURE_MIN_FILTER, GL.GL_LINEAR)
+                    GL.glTexParameteri(GL.GL_TEXTURE_CUBE_MAP, GL.GL_TEXTURE_MAG_FILTER, GL.GL_LINEAR)
+                    GL.glTexParameteri(GL.GL_TEXTURE_CUBE_MAP, GL.GL_TEXTURE_WRAP_S, GL.GL_CLAMP_TO_EDGE)
+                    GL.glTexParameteri(GL.GL_TEXTURE_CUBE_MAP, GL.GL_TEXTURE_WRAP_T, GL.GL_CLAMP_TO_EDGE)
+                    GL.glTexParameteri(GL.GL_TEXTURE_CUBE_MAP, GL.GL_TEXTURE_WRAP_R, GL.GL_CLAMP_TO_EDGE)
                     i += 1
+                print("Skybox textures loaded.")
 
-        # setup & upload texture to GPU, bind it to shader name 'cube_map'
-        texture = CubeMapTex(tex_path)
+        mesh = Mesh(shader, attributes=dict(position=coords))
+        texture = Texture(tex_path)
         super().__init__(mesh, cube_map=texture)
--- viewer.py
+++ viewer.py
@@ -3,22 +3,22 @@
 Python OpenGL practical application.
 """
 
-import sys                          # for system arguments
+import sys  # for system arguments
 
 # External, non built-in modules
-import OpenGL.GL as GL              # standard Python OpenGL wrapper
-import numpy as np                  # all matrix manipulations & OpenGL args
-import glfw                         # lean window system wrapper for OpenGL
+import OpenGL.GL as GL  # standard Python OpenGL wrapper
+import numpy as np  # all matrix manipulations & OpenGL args
+import glfw  # lean window system wrapper for OpenGL
 
 from core import Shader, Mesh, Viewer, Node, load
-from skybox import CubeMapTexture
+from skybox import SkyBox
 from transform import translate, identity, rotate, scale
-from texture import Terrain, TexturedSphere, TexturedCylinder, TexturedPlane, TexturedTree, ForestTerrain,Texture
-
+from texture import Terrain, TexturedSphere, TexturedCylinder, TexturedPlane, TexturedTree, ForestTerrain, Texture
 
 
 class Axis(Mesh):
     """ Axis object useful for debugging coordinate frames """
+
     def __init__(self, shader):
         pos = ((0, 0, 0), (1, 0, 0), (0, 0, 0), (0, 1, 0), (0, 0, 0), (0, 0, 1))
         col = ((1, 0, 0), (1, 0, 0), (0, 1, 0), (0, 1, 0), (0, 0, 1), (0, 0, 1))
@@ -30,6 +30,7 @@ class Axis(Mesh):
 
 class Triangle(Mesh):
     """Hello triangle object"""
+
     def __init__(self, shader):
         position = np.array(((0, .5, 0), (-.5, -.5, 0), (.5, -.5, 0)), 'f')
         color = np.array(((1, 0, 0), (0, 1, 0), (0, 0, 1)), 'f')
@@ -45,7 +46,6 @@ class Triangle(Mesh):
             self.color = (0, 0, 0)
 
 
-
 # -------------- main program and scene setup --------------------------------
 def main():
     """ create a window, add scene objects, then run rendering loop """
@@ -56,15 +56,17 @@ def main():
     shaderTexture = Shader("Shaders/texture.vert", "Shaders/texture.frag")
     shaderLight = Shader("Shaders/phong.vert", "Shaders/phong.frag")
     skyboxShader = Shader("Shaders/skybox.vert", "Shaders/skybox.frag")
-    
-    light_dir = (-1,-1,-1)
+
+    light_dir = (-1, -1, -1)
 
     # place instances of our basic objects
     viewer.add(*[mesh for file in sys.argv[1:] for mesh in load(file, shader, light_dir=light_dir)])
     if len(sys.argv) < 2:
-        # viewer.add(Axis(shaderTexture))
-        viewer.add(CubeMapTexture(skyboxShader, "Textures/skybox/"))
-        viewer.add(ForestTerrain(shader=shaderLight, terrainTexture=Texture("Textures/grass.png"),trunkTextures=Texture("Textures/tronc.jpg"), leavesTextures=Texture("Textures/leaves.jpg"), light_dir=light_dir))
+        viewer.add(Axis(shaderTexture))
+        viewer.add(SkyBox(skyboxShader, "Textures/skybox/"))
+        viewer.add(ForestTerrain(shader=shaderLight, terrainTexture=Texture("Textures/grass.png"),
+                                 trunkTextures=Texture("Textures/tronc.jpg"),
+                                 leavesTextures=Texture("Textures/leaves.jpg"), light_dir=light_dir))
         print('Usage:\n\t%s [3dfile]*\n\n3dfile\t\t the filename of a model in'
               ' format supported by assimp.' % (sys.argv[0],))
 
@@ -73,4 +75,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()                     # main function keeps variables locally scoped
+    main()  # main function keeps variables locally scoped
